16c98159826292b6d2db1afa9a15a6d3a800acb1
钱鹏 <jess.qian@55haitao.com>
2021-04-25 15:24:56 +0800

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'peng/demo1',
+  ),
+  'versions' => 
+  array (
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+    ),
+    'peng/demo1' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
